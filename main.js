6c12ead5e82e3b64787bb56dc0258f3a9b94620b
Mario González Santoyo <mario_gnzsa@hotmail.com>
2021-05-11 17:45:05 -0500

    Changes in nav colors

--- main.js
+++ main.js
@@ -7,13 +7,29 @@ $(".menu-text").hover(function(){
     $(this).css("color", "white");
 });
 
+// var intervalId = setInterval(function() {
+//     $("#logo").css("color", generateRandomColor());
+//     $("#git-icon").css("color", generateRandomColor());
+//     $("#linkedin-icon").css("color", generateRandomColor());
+//     $("#facebook-icon").css("color", generateRandomColor());
+//     $("#instagram-icon").css("color", generateRandomColor());
+// }, 100);
+let r = 255 * Math.random() | 50;
+let g = 255 * Math.random() | 50;
+let b = 255 * Math.random() | 50;
 var intervalId = setInterval(function() {
-    $("#logo").css("color", generateRandomColor());
-    $("#git-icon").css("color", generateRandomColor());
-    $("#linkedin-icon").css("color", generateRandomColor());
-    $("#facebook-icon").css("color", generateRandomColor());
-    $("#instagram-icon").css("color", generateRandomColor());
-}, 1500);
+    if(r > 255) r = 0;
+    if(g > 255) g = 0;
+    if(b > 255) b = 0;
+    $("#logo").css("color", 'rgb('+r.toString()+','+g.toString()+','+b.toString()+')');
+    $("#git-icon").css("color", 'rgb('+r.toString()+','+g.toString()+','+b.toString()+')');
+    $("#linkedin-icon").css("color", 'rgb('+r.toString()+','+g.toString()+','+b.toString()+')');
+    $("#facebook-icon").css("color", 'rgb('+r.toString()+','+g.toString()+','+b.toString()+')');
+    $("#instagram-icon").css("color", 'rgb('+r.toString()+','+g.toString()+','+b.toString()+')');
+    r++;
+    g++;
+    b++;
+}, 5);
 
 var intervalId2 = setInterval(function() {
     timeline2.fromTo('#static', {opacity: 0}, {opacity: 0.2, duration: 2});
